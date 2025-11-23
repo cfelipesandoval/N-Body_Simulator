e9cd46d01af00a74839939962a7158664e7a6fd7
FelipeDoesHisThing <89559893+FelipeDoesHisThing@users.noreply.github.com>
2025-11-23 17:39:07 -0500

    updated default window size

--- build/launch-n_body_simulator.cmd
+++
@@ -1,9 +0,0 @@
-cd C:/Users/cfsan/Desktop/ECE6122/Final Project/n_body_simulator/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"C:\Users\cfsan\Desktop\ECE6122\Final Project\build\n_body_simulator.exe"  
-pause
